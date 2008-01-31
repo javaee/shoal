8cb63936112f4731cea8ca5393886bfba91e2a5a
Sheetal Vartak <sheetalv@java.net>
2008-01-31 21:53:44 +0000

    adding a script to run the test to test the newly added API for group shut down.
    svn path=/trunk/; revision=489

---
+++ gms/runcheckgroupshutdown.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+#
+# Copyright 2004-2005 Sun Microsystems, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+ #
+ # The contents of this file are subject to the terms
+ # of the Common Development and Distribution License
+ # (the License).  You may not use this file except in
+ # compliance with the License.
+ #
+ # You can obtain a copy of the license at
+ # https://shoal.dev.java.net/public/CDDLv1.0.html
+ #
+ # See the License for the specific language governing
+ # permissions and limitations under the License.
+ #
+ # When distributing Covered Code, include this CDDL
+ # Header Notice in each file and include the License file
+ # at
+ # If applicable, add the following below the CDDL Header,
+ # with the fields enclosed by brackets [] replaced by
+ # you own identifying information:
+ # "Portions Copyrighted [year] [name of copyright owner]"
+ #
+ # Copyright 2006 Sun Microsystems, Inc. All rights reserved.
+ #
+publish_home=./dist
+lib_home=./lib
+
+java -Dcom.sun.management.jmxremote -cp ${publish_home}/shoal-gms.jar:${lib_home}/jxta.jar:${lib_home}/bcprov-jdk14.jar com.sun.enterprise.ee.cms.tests.checkgroupshutdown.CheckIfGroupShuttingDownTest;
+
