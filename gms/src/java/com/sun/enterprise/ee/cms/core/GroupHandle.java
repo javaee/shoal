f2cab506acb2610c3b6efccd9817d179721e6a74
hamada <hamada@localhost>
2009-05-06 16:21:40 +0000

    - Adds JavaDocs about sending messages larger than 64K
    svn path=/trunk/; revision=693

--- gms/src/java/com/sun/enterprise/ee/cms/core/GroupHandle.java
+++ gms/src/java/com/sun/enterprise/ee/cms/core/GroupHandle.java
@@ -46,10 +46,10 @@ import java.util.List;
  * individual members or a list of members, and also to specific components
  * within the target member or group.
  * Provides a reference to the DistributedStateCache, and APIs for
- * FailureFencing. 
+ * FailureFencing.
  *
  * @author Shreedhar Ganapathy
- * Date: Jan 12, 2004
+ *         Date: Jan 12, 2004
  * @version $Revision$
  */
 public interface GroupHandle {
@@ -59,8 +59,13 @@ public interface GroupHandle {
      * carrying the payload. Specifying a null component name would
      * result in the message being delivered to all registered
      * components in the target member instance.
+     * <p/>
+     * Note: when utilizing multicast, message size should not exceed the underlying communication
+     * provider message limits (64K).  When sending messages larger than the limit, consider sending
+     * the message to each individual member
+     *
      * @param targetComponentName target name
-     * @param message the message to send
+     * @param message             the message to send
      * @throws GMSException - any exception while sending message wrapped into GMSException
      */
     void sendMessage(String targetComponentName, byte[] message) throws GMSException;
@@ -73,51 +78,56 @@ public interface GroupHandle {
      * a null component name would result in the message being
      * delivered to all registered components in the target member
      * instance.
-     * @param targetServerToken targetServerToken representing the recipient member's id
+     *
+     * @param targetServerToken   targetServerToken representing the recipient member's id
      * @param targetComponentName target name
-     * @param message the message to send
+     * @param message             the message to send
      * @throws GMSException - any exception while sending message wrapped into GMSException
      */
-     void sendMessage(String targetServerToken, String targetComponentName,
-                      byte[] message) throws GMSException;
+    void sendMessage(String targetServerToken, String targetComponentName,
+                     byte[] message) throws GMSException;
 
     /**
      * Sends a message to a list of members in the group. An empty list would
      * have the same effect as sending the message to the entire group
      *
-     * @param targetServerTokens List of target server tokens
+     * @param targetServerTokens  List of target server tokens
      * @param targetComponentName a component in the target members to which message is addressed.
-     * @param message - the payload
+     * @param message             - the payload
      * @throws GMSException - any exception while sending message wrapped into GMSException
      */
-     void sendMessage(List<String> targetServerTokens, String targetComponentName,
-                      byte[] message) throws GMSException;
-    
+    void sendMessage(List<String> targetServerTokens, String targetComponentName,
+                     byte[] message) throws GMSException;
+
     /**
      * returns a DistributedStateCache object that provides the ability to
      * set and retrieve CachedStates.
-     * @see DistributedStateCache
+     *
      * @return DistributedStateCache
+     * @see DistributedStateCache
      */
-     DistributedStateCache getDistributedStateCache();
+    DistributedStateCache getDistributedStateCache();
 
     /**
      * returns a List of strings containing the current core members
      * in the group
+     *
      * @return List
      */
     List<String> getCurrentCoreMembers();
 
     /**
      * * returns only the members that are in ALIVE or READY state
-     * @return  List
+     *
+     * @return List
      */
     List<String> getCurrentAliveOrReadyMembers();
 
     /**
      * API for giving the state of the member
      * Calls #getMemberState(String, long, long) with implementation default values for <code>threshold</code> and <code>timeout</code>.
-     * @param member
+     *
+     * @param member the group member
      * @return the state of the <code>member</code>.
      */
     MemberStates getMemberState(String member);
@@ -125,39 +135,43 @@ public interface GroupHandle {
     /**
      * Returns the state of the member.
      * The parameters <code>threshold</code> and <code>timeout</code> enable the caller to tune this
-     * lookup between accuracy and time it will take to complete the call.  It is lowest cost to just return 
+     * lookup between accuracy and time it will take to complete the call.  It is lowest cost to just return
      * the local computed concept for a member's state. <code>threshold</code> parameter controls this.
      * If the local state is stale, then the <code>timeout</code> parameter enables one to control how
      * long they are willing to wait for more accurate state information from the member itself.
-     * @param member
+     *
+     * @param member    the group member
      * @param threshold allows caller to specify how up-to-date the member state information has to be.
-     *  The  larger this value, the better chance that this method just returns the local concept of this member's state.
-     * The smaller this value, the better chance that the local state is not fresh enough and the method will find out directly
-     * from the instance what its current state is.
-     * @param timeout is the time for which the caller instance should wait to get the state from the concerned member
-     * via a network call.
-     * if timeout and threshold are both 0, then the default values are used
-     * if threshold is 0, then a network call is made to get the state of the member
-     * if timeout is 0, then the caller instance checks for the state of the member stored with it within the
-     * given threshold
+     *                  The  larger this value, the better chance that this method just returns the local concept of this member's state.
+     *                  The smaller this value, the better chance that the local state is not fresh enough and the method will find out directly
+     *                  from the instance what its current state is.
+     * @param timeout   is the time for which the caller instance should wait to get the state from the concerned member
+     *                  via a network call.
+     *                  if timeout and threshold are both 0, then the default values are used
+     *                  if threshold is 0, then a network call is made to get the state of the member
+     *                  if timeout is 0, then the caller instance checks for the state of the member stored with it within the
+     *                  given threshold
      * @return the state of the member
-     * Returns UNKNOWN when the local state for the member is considered stale (determined by threshold value)
-     * and the network invocation to get the member state times out before getting a reply from the member of what its state is.
+     *         Returns UNKNOWN when the local state for the member is considered stale (determined by threshold value)
+     *         and the network invocation to get the member state times out before getting a reply from the member of what its state is.
      */
 
     MemberStates getMemberState(String member, long threshold, long timeout);
+
     /**
      * returns a List of strings containing the current group membership including
      * spectator members.
+     *
      * @return List
      */
-     List<String> getAllCurrentMembers();
+    List<String> getAllCurrentMembers();
 
     /**
      * returns a List of strings containing the current core members
      * in the group. Each entry contains a "::" delimiter after the
      * member token id. After the delimited is a string representation of the
      * long value that stands for that member's startup timestamp.
+     *
      * @return List
      */
     List<String> getCurrentCoreMembersWithStartTimes();
@@ -167,9 +181,10 @@ public interface GroupHandle {
      * spectator members. Each entry contains a "::" delimiter after the
      * member token id. After the delimited is a string representation of the
      * long value that stands for that member's startup timestamp.
+     *
      * @return List
      */
-     List<String> getAllCurrentMembersWithStartTimes();
+    List<String> getAllCurrentMembersWithStartTimes();
 
     //FAILURE FENCING RELATED API
     /**
@@ -188,37 +203,37 @@ public interface GroupHandle {
      * method below.
      * <p>Raising the fence, places an entry into a distributed datastructure
      * that is accessed by other members during their startup
-     *
+     * <p/>
      * <p>Direct calls to this method is meant only for self-recovering clients.
      * For clients that perform recovery as a surrogate for a failed instance,
      * the FailureRecoverySignal's acquire() method should be called. That
      * method has the effect of raising the fence and performing any other state
      * management operation that may be added in future.
-     * @param componentName
-     * @param failedMemberToken
-     * @throws GMSException
      *
+     * @param componentName     the component channel name
+     * @param failedMemberToken the failed member
+     * @throws GMSException if an error occurs
      */
-    void raiseFence (String componentName,
-                     String failedMemberToken ) throws GMSException;
+    void raiseFence(String componentName,
+                    String failedMemberToken) throws GMSException;
 
     /**
      * Enables the caller to lower a logical fence that was earlier raised on
      * a target member component. This is typically done when the operation
      * being performed on the target member component has now completed.
-     *
+     * <p/>
      * <p>Direct calls to this method is meant only for self-recovering clients.
      * For clients that perform recovery as a surrogate for a failed instance,
      * the FailureRecoverySignal's release() method should be called. That
      * method has the effect of lowering the fence and performing any other
      * state management operation that may be added in future.
      *
-     * @param componentName
-     * @param failedMemberToken
-     * @throws GMSException
+     * @param componentName     the component channel name
+     * @param failedMemberToken the failed member
+     * @throws GMSException if an error occurs
      */
-    void lowerFence (String componentName,
-                     String failedMemberToken ) throws GMSException;
+    void lowerFence(String componentName,
+                    String failedMemberToken) throws GMSException;
 
     /**
      * Provides the status of a member component's fence, if any.
@@ -238,42 +253,46 @@ public interface GroupHandle {
      * mean that the client component can continue with its lifecycle startup
      * per its normal startup policies.
      *
-     * @param componentName
-     * @param memberToken
+     * @param componentName the component channel name
+     * @param memberToken   the member
      * @return boolean
      */
     boolean isFenced(String componentName, String memberToken);
 
     /**
      * Checks if a member is alive
-     * @param memberToken
+     *
+     * @param memberToken the member
      * @return boolean
      */
     boolean isMemberAlive(String memberToken);
 
     /**
      * Return the leader of the group
+     *
      * @return String representing the member identity token of the group leader
      */
     String getGroupLeader();
 
     /**
      * This is a check to find out if this peer is a group leader.
+     *
      * @return true if this peer is the group leader
      */
     boolean isGroupLeader();
-    
+
     /**
      * GMS WATCHDOG member reports <code>serverToken</code> has been observed to have failed to this GMS group.
-     * <p>
+     * <p/>
      * This is merely a hint and the GMS system validates that the GMS member has truely failed using
      * the same verification algorithm used when GMS heartbeat reports a member is INDOUBT and SUSPECTED of
      * failure.
-     * <p>
+     * <p/>
      * Allows for enhanced GMS failure detection by external control entities (one example is NodeAgent of Glassfish Application Server.)
      * Only a GMS MemberType of WATCHDOG is allowed to broadcast to all members of a group that this <code>serverToken</code> has likely failed.
-     * @param serverToken     failed member
-     * @throws GMSException   if called by a member that is not a WATCHDOG member or if serverToken is not currently running in group.
+     *
+     * @param serverToken failed member
+     * @throws GMSException if called by a member that is not a WATCHDOG member or if serverToken is not currently running in group.
      */
     void announceWatchdogObservedFailure(String serverToken) throws GMSException;
 }
