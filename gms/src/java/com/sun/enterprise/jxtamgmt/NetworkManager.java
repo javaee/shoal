4e7964744ec5e3469f4c499c4f981d0c46879a17
Joe Fialli <joe.fialli@oracle.com>
2009-12-14 20:55:04 +0000

    Issue number: shoal issue 94 Fix IndexOutOfBounds exception in NetworkManger log.config statement.
    svn path=/trunk/; revision=758

--- gms/src/java/com/sun/enterprise/jxtamgmt/NetworkManager.java
+++ gms/src/java/com/sun/enterprise/jxtamgmt/NetworkManager.java
@@ -181,7 +181,7 @@ public class NetworkManager implements RendezvousListener {
                     //this object has only one address in it, so add it to the list
                     rendezvousSeedURIs.add(((String) virtualMulticastURIList));
                 }
-                LOG.config("VIRTUAL_MULTICAST_URI_LIST=" + virtualMulticastURIList + " rendezvousSeedURIs.get(0)=" + rendezvousSeedURIs.get(1));
+                LOG.config("VIRTUAL_MULTICAST_URI_LIST=" + virtualMulticastURIList + " rendezvousSeedURIs.get(0)=" + rendezvousSeedURIs.get(0));
             }
             Object isVirtualMulticastNode = properties.get(IS_BOOTSTRAPPING_NODE.toString());
             if (isVirtualMulticastNode != null) {
