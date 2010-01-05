267a7eef6867daec52780f2fc19756a44292bb74
Stephen Dimilla <stephen.dimilla@oracle.com>
2010-01-05 21:16:28 +0000

    enhancements to detect when test starts, also do more enhanced error detection
    svn path=/branches/SHOAL_1_1_ABSTRACTING_TRANSPORT/; revision=768

--- gms/runsenderreceivertest.sh
+++ gms/runsenderreceivertest.sh
@@ -70,6 +70,51 @@ count=\`grep "Testing Complete" *log | wc -l | sed -e 's/ //g' \`
 sleep 5
 done
 \$ECHO  ", \$count"
+\$ECHO  "The following logs contain failures:"
+\$ECHO  "==============="
+grep "FAILED" instance*log
+\$ECHO  "==============="
+\$ECHO  "The following are the time results for sending messages:"
+grep "Sending Messages Time data" instance*log
+\$ECHO  "==============="
+\$ECHO  "The following are the time results for receiving messages:"
+grep "Receiving Messages Time data" instance*log
+\$ECHO  "==============="
+\$ECHO  "The following are exceptions found in the logs:"
+\$ECHO  "==============="
+grep "Exception" *log
+\$ECHO  "==============="
+\$ECHO  "The following are SEVERE messages found in the logs:"
+\$ECHO  "==============="
+grep "SEVERE" *log
+\$ECHO  "==============="
+
+exit 0
+
+ENDSCRIPT
+#=====================================================================
+
+#=====================================================================
+# Create the script monitors that monitors when  testing is complete
+#=====================================================================
+rm -rf /tmp/script3
+cat << ENDSCRIPT > /tmp/script3
+#!/bin/sh +x
+
+ECHO=\`which echo\`
+num=\`ls -al *log |grep -v server.log | wc -l | sed -e 's/ //g' \`
+count=\`grep "All members have joined the group" *log | wc -l | sed -e 's/ //g' \`
+\$ECHO "Waiting for the all (\$num) instances to join group"
+\$ECHO -n "\$count"
+while [ \$count -ne \$num ]
+do
+\$ECHO -n ",\$count"
+count=\`grep "All members have joined the group" *log | wc -l | sed -e 's/ //g' \`
+sleep 5
+done
+\$ECHO  ", \$count"
+\$ECHO "All (\$num) instances have joined the group, testing will now begin"
+
 exit 0
 
 ENDSCRIPT
@@ -93,32 +138,41 @@ exit 1
 
 chmod 755 /tmp/script1
 chmod 755 /tmp/script2
+chmod 755 /tmp/script3
 rm -rf instance*.log server.log
 
 if [ "$1" == "-h" ]; then
 usage
 fi
 
+# this value must match the number of /tmp/script1 calls below
+numInstances="10"
+msgSize=1024
+numOfMsgs=7000
 
+$ECHO "Message size=${msgSize}"
+$ECHO "Number of messages=${numOfMsgs}"
 
 $ECHO "Starting DAS"
-/tmp/script1 server 10 >& server.log &
+/tmp/script1 server ${numInstances} >& server.log &
 # give time for the DAS to start
 sleep 5
-$ECHO "Starting instances"
-/tmp/script1 instance101 10 1024 7000 9130 9160 >& instance101.log &
-/tmp/script1 instance102 10 1024 7000 9160 9190 >& instance102.log &
-/tmp/script1 instance103 10 1024 7000 9230 9260 >& instance103.log &
-/tmp/script1 instance104 10 1024 7000 9261 9290 >& instance104.log &
-/tmp/script1 instance105 10 1024 7000 9330 9360 >& instance105.log &
-/tmp/script1 instance106 10 1024 7000 9361 9390 >& instance106.log &
-/tmp/script1 instance107 10 1024 7000 9430 9460 >& instance107.log &
-/tmp/script1 instance108 10 1024 7000 9461 9490 >& instance108.log &
-/tmp/script1 instance109 10 1024 7000 9530 9560 >& instance109.log &
-/tmp/script1 instance110 10 1024 7000 9561 9590 >& instance110.log &
+$ECHO "Starting ${numInstances} instances"
 
+/tmp/script1 instance101 ${numInstances} ${msgSize} ${numOfMsgs} 9130 9160 >& instance101.log &
+/tmp/script1 instance102 ${numInstances} ${msgSize} ${numOfMsgs} 9160 9190 >& instance102.log &
+/tmp/script1 instance103 ${numInstances} ${msgSize} ${numOfMsgs} 9230 9260 >& instance103.log &
+/tmp/script1 instance104 ${numInstances} ${msgSize} ${numOfMsgs} 9261 9290 >& instance104.log &
+/tmp/script1 instance105 ${numInstances} ${msgSize} ${numOfMsgs} 9330 9360 >& instance105.log &
+/tmp/script1 instance106 ${numInstances} ${msgSize} ${numOfMsgs} 9361 9390 >& instance106.log &
+/tmp/script1 instance107 ${numInstances} ${msgSize} ${numOfMsgs} 9430 9460 >& instance107.log &
+/tmp/script1 instance108 ${numInstances} ${msgSize} ${numOfMsgs} 9461 9490 >& instance108.log &
+/tmp/script1 instance109 ${numInstances} ${msgSize} ${numOfMsgs} 9530 9560 >& instance109.log &
+/tmp/script1 instance110 ${numInstances} ${msgSize} ${numOfMsgs} 9561 9590 >& instance110.log &
 
 # give time for the instances to start
 sleep 3
+# monitor for the testing to begin
+/tmp/script3
 # monitor when the testing is complete
 /tmp/script2
