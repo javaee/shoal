494036e88394bd2afbd7996bf3471d7bac3013bc
Stephen Dimilla <stephen.dimilla@oracle.com>
2010-04-23 15:19:13 +0000

    change logging to independently control test logging from shoal logging
    svn path=/branches/SHOAL_1_1_ABSTRACTING_TRANSPORT/; revision=897

--- gms/runsimulatecluster.sh
+++ gms/runsimulatecluster.sh
@@ -19,28 +19,31 @@ MULTICASTADDRESS=229.9.1.`./randomNumber.sh`
 echo ${MULTICASTADDRESS} > ./currentMulticastAddress.txt
 MULTICASTPORT=2299
 
-
+TEST_LOG_LEVEL=WARNING
+SHOALGMS_LOG_LEVEL=INFO
 
 echo "Starting admin"
-./rungmsdemo.sh server ${GROUPNAME} SPECTATOR 0 INFO ${TRANSPORT} -ts 9130 -te 9160 -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT} > ${LOGS_DIR}/server.log 2>&1 &
+./rungmsdemo.sh server ${GROUPNAME} SPECTATOR 0 ${SHOALGMS_LOG_LEVEL} ${TRANSPORT} -tl ${TEST_LOG_LEVEL} -ts 9130 -te 9160 -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT} > ${LOGS_DIR}/server.log 2>&1 &
 sleep 5
 echo "Starting CORE members"
-./rungmsdemo.sh instance01 ${GROUPNAME} CORE 0 CONFIG ${TRANSPORT} -ts 9161 -te 9190 -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT} > ${LOGS_DIR}/instance01.log 2>&1 &
-./rungmsdemo.sh instance02 ${GROUPNAME} CORE 0 CONFIG ${TRANSPORT} -ts 9230 -te 9260 -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT} > ${LOGS_DIR}/instance02.log 2>&1 &
-./rungmsdemo.sh instance03 ${GROUPNAME} CORE 0 CONFIG ${TRANSPORT} -ts 9261 -te 9290 -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT} > ${LOGS_DIR}/instance03.log 2>&1 &
-./rungmsdemo.sh instance04 ${GROUPNAME} CORE 0 CONFIG ${TRANSPORT} -ts 9330 -te 9360 -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT} > ${LOGS_DIR}/instance04.log 2>&1 &
-./rungmsdemo.sh instance05 ${GROUPNAME} CORE 0 CONFIG ${TRANSPORT} -ts 9361 -te 9390 -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT} > ${LOGS_DIR}/instance05.log 2>&1 &
-./rungmsdemo.sh instance06 ${GROUPNAME} CORE 0 CONFIG ${TRANSPORT} -ts 9430 -te 9460 -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT} > ${LOGS_DIR}/instance06.log 2>&1 &
-./rungmsdemo.sh instance07 ${GROUPNAME} CORE 0 CONFIG ${TRANSPORT} -ts 9461 -te 9490 -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT} > ${LOGS_DIR}/instance07.log 2>&1 &
-./rungmsdemo.sh instance08 ${GROUPNAME} CORE 0 CONFIG ${TRANSPORT} -ts 9530 -te 9560 -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT} > ${LOGS_DIR}/instance08.log 2>&1 &
-./rungmsdemo.sh instance09 ${GROUPNAME} CORE 0 CONFIG ${TRANSPORT} -ts 9561 -te 9590 -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT} > ${LOGS_DIR}/instance09.log 2>&1 &
-./rungmsdemo.sh instance10 ${GROUPNAME} CORE 0 CONFIG ${TRANSPORT} -ts 9631 -te 9690 -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT} > ${LOGS_DIR}/instance10.log 2>&1 &
+./rungmsdemo.sh instance01 ${GROUPNAME} CORE 0 ${SHOALGMS_LOG_LEVEL} ${TRANSPORT} -tl ${TEST_LOG_LEVEL} -ts 9161 -te 9190 -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT} > ${LOGS_DIR}/instance01.log 2>&1 &
+./rungmsdemo.sh instance02 ${GROUPNAME} CORE 0 ${SHOALGMS_LOG_LEVEL} ${TRANSPORT} -tl ${TEST_LOG_LEVEL} -ts 9230 -te 9260 -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT} > ${LOGS_DIR}/instance02.log 2>&1 &
+./rungmsdemo.sh instance03 ${GROUPNAME} CORE 0 ${SHOALGMS_LOG_LEVEL} ${TRANSPORT} -tl ${TEST_LOG_LEVEL} -ts 9261 -te 9290 -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT} > ${LOGS_DIR}/instance03.log 2>&1 &
+./rungmsdemo.sh instance04 ${GROUPNAME} CORE 0 ${SHOALGMS_LOG_LEVEL} ${TRANSPORT} -tl ${TEST_LOG_LEVEL} -ts 9330 -te 9360 -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT} > ${LOGS_DIR}/instance04.log 2>&1 &
+./rungmsdemo.sh instance05 ${GROUPNAME} CORE 0 ${SHOALGMS_LOG_LEVEL} ${TRANSPORT} -tl ${TEST_LOG_LEVEL} -ts 9361 -te 9390 -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT} > ${LOGS_DIR}/instance05.log 2>&1 &
+./rungmsdemo.sh instance06 ${GROUPNAME} CORE 0 ${SHOALGMS_LOG_LEVEL} ${TRANSPORT} -tl ${TEST_LOG_LEVEL} -ts 9430 -te 9460 -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT} > ${LOGS_DIR}/instance06.log 2>&1 &
+./rungmsdemo.sh instance07 ${GROUPNAME} CORE 0 ${SHOALGMS_LOG_LEVEL} ${TRANSPORT} -tl ${TEST_LOG_LEVEL} -ts 9461 -te 9490 -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT} > ${LOGS_DIR}/instance07.log 2>&1 &
+./rungmsdemo.sh instance08 ${GROUPNAME} CORE 0 ${SHOALGMS_LOG_LEVEL} ${TRANSPORT} -tl ${TEST_LOG_LEVEL} -ts 9530 -te 9560 -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT} > ${LOGS_DIR}/instance08.log 2>&1 &
+./rungmsdemo.sh instance09 ${GROUPNAME} CORE 0 ${SHOALGMS_LOG_LEVEL} ${TRANSPORT} -tl ${TEST_LOG_LEVEL} -ts 9561 -te 9590 -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT} > ${LOGS_DIR}/instance09.log 2>&1 &
+./rungmsdemo.sh instance10 ${GROUPNAME} CORE 0 ${SHOALGMS_LOG_LEVEL} ${TRANSPORT} -tl ${TEST_LOG_LEVEL} -ts 9631 -te 9690 -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT} > ${LOGS_DIR}/instance10.log 2>&1 &
 
 echo "Waiting for group [${GROUPNAME}] to complete startup"
-./gms_admin.sh waits ${GROUPNAME} -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT}
+# we do not want test or shoal output unless we really needit, there we set both types of logging to the same value
+./gms_admin.sh waits ${GROUPNAME} -tl ${TEST_LOG_LEVEL} -sl ${TEST_LOG_LEVEL} -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT}
 
 echo "Group startup has completed, shutting down group [${GROUPNAME}]"
-./gms_admin.sh stopc ${GROUPNAME} -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT}
+# we do not want test or shoal output unless we really needit, there we set both types of logging to the same value
+./gms_admin.sh stopc ${GROUPNAME} -tl ${TEST_LOG_LEVEL} -sl ${TEST_LOG_LEVEL} -ma ${MULTICASTADDRESS} -mp ${MULTICASTPORT}
 
 
 # The old way of running this test scenario
