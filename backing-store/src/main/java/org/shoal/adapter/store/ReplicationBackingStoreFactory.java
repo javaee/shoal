864109659bcbfb1f5922075c32b956fe755f53ac
Mahesh Kannan <mahesh.kannan@oracle.com>
2010-06-13 19:51:56 +0000

    Added a new createBackingStore method that initializes store using BackingStoreConfiguration
    svn path=/branches/SHOAL_1_1_ABSTRACTING_TRANSPORT/; revision=989

--- backing-store/src/main/java/org/shoal/adapter/store/ReplicationBackingStoreFactory.java
+++ backing-store/src/main/java/org/shoal/adapter/store/ReplicationBackingStoreFactory.java
@@ -1,14 +1,45 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2010 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
 package org.shoal.adapter.store;
 
-import org.glassfish.ha.store.spi.BackingStore;
-import org.glassfish.ha.store.spi.BackingStoreException;
-import org.glassfish.ha.store.spi.BackingStoreFactory;
-import org.glassfish.ha.store.spi.BatchBackingStore;
+import org.glassfish.ha.store.api.*;
 import org.jvnet.hk2.annotations.Service;
-import org.shoal.ha.cache.api.DataStore;
-import org.shoal.ha.cache.api.DataStoreFactory;
 
-import java.util.Properties;
+import java.io.Serializable;
 
 /**
  * @author Mahesh Kannan
@@ -17,21 +48,21 @@ import java.util.Properties;
 public class ReplicationBackingStoreFactory
     implements BackingStoreFactory {
 
-     @Override
-    public <K, V> BackingStore<K, V> createBackingStore(String storeName, Class<K> keyClazz, Class<V> vClazz, Properties env) throws BackingStoreException {
-        InMemoryBackingStore<K, V> bStore = new InMemoryBackingStore<K, V>();
-        Properties props = new Properties(env);
-        String instanceName = (String) props.get("instance.name");
-        String groupName = (String) props.get("group.name");
-        bStore.initialize(storeName, keyClazz, vClazz, props);
-        DataStore<K, V> ds = (DataStore<K, V>) DataStoreFactory.createDataStore(storeName, instanceName, groupName,
-                keyClazz, vClazz, Thread.currentThread().getContextClassLoader());
 
+    @Override
+    public <K extends Serializable, V extends Serializable> BackingStore<K, V> createBackingStore(BackingStoreConfiguration<K, V> conf) throws BackingStoreException {
+
+        InMemoryBackingStore<K, V> bStore = new InMemoryBackingStore<K, V>();
+        bStore.initialize(conf);
         return bStore;
     }
 
     @Override
-    public BatchBackingStore createBatchBackingStore(Properties env) throws BackingStoreException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    public BackingStoreTransaction createBackingStoreTransaction() {
+        return new BackingStoreTransaction() {
+            @Override
+            public void commit() throws BackingStoreException {    
+            }
+        };
     }
 }
