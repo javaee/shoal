bb8e6b428b65db6a7e1d9880a6878ef3acc3ef30
Joe Fialli <joe.fialli@oracle.com>
2011-01-04 21:32:05 +0000

    Fix for glasfish isue 15417.
    Reviewed by Bobby Bissett.
    Fix is to not have comma in "port number" when it is printed out.
    
    Fix From:
    Will use port 2,048
    
    To:
    Will use port 2048
    
    
    svn path=/branches/SHOAL_1_1_ABSTRACTING_TRANSPORT/; revision=1413

--- gms/impl/src/main/java/com/sun/enterprise/gms/tools/MulticastTester.java
+++ gms/impl/src/main/java/com/sun/enterprise/gms/tools/MulticastTester.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -83,7 +83,7 @@ public class MulticastTester {
         }
 
         StringBuilder out = new StringBuilder();
-        out.append(sm.get("port.set", mcPort)).append("\n");
+        out.append(sm.get("port.set", Integer.toString(mcPort))).append("\n");
         out.append(sm.get("address.set", mcAddress)).append("\n");
         out.append(sm.get("bind.int.set", bindInterface)).append("\n");
         out.append(sm.get("period.set", msgPeriodInMillis)).append("\n");
