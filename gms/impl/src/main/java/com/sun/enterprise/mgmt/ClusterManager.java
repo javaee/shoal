e6be9f0d0194625f33a0476e0ad5f8f2f543336b
Jayasheelan Kumar <jayasheelan.kumar@oracle.com>
2018-03-01 15:21:06 +0530

    Revert "Updating the Copyright details"
    This reverts commit 1e21087ddc87c35939d5cdf0710bfea4e674ecbf.

--- gms/impl/src/main/java/com/sun/enterprise/mgmt/ClusterManager.java
+++ gms/impl/src/main/java/com/sun/enterprise/mgmt/ClusterManager.java
@@ -1,19 +1,19 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2017 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
  * and Distribution License("CDDL") (collectively, the "License").  You
  * may not use this file except in compliance with the License.  You can
  * obtain a copy of the License at
- * https://oss.oracle.com/licenses/CDDL+GPL-1.1
- * or LICENSE.txt.  See the License for the specific
+ * https://glassfish.dev.java.net/public/CDDL2011+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
  *
  * When distributing the software, include this License Header Notice in each
- * file and include the License file at LICENSE.txt.
+ * file and include the License file at packager/legal/LICENSE.txt.
  *
  * GPL Classpath Exception:
  * Oracle designates this particular file as subject to the "Classpath"
@@ -21,10 +21,10 @@
  * file that accompanied this code.
  *
  * Modifications:
- * If applicable, add the following below the License Header, with the fields
+ * If applicable, add the following below the License Header,2011 with the fields
  * enclosed by brackets [] replaced by your own identifying information:
  * "Portions Copyright [year] [name of copyright owner]"
- *
+ *20112011
  * Contributor(s):
  * If you wish your version of this file to be governed by only the CDDL or
  * only the GPL Version 2, indicate your decision by adding "[Contributor]
@@ -32,7 +32,7 @@
  * Version 2] license."  If you don't indicate a single choice of license, a
  * recipient has the option to distribute your version of this file under
  * either the CDDL, the GPL Version 2 or to extend the choice of license to
- * its licensees as provided above.  However, if you add GPL Version 2 code
+ * its licensees as provided above.  However, if2011 you add GPL Version 2 code
  * and therefore, elected the GPL Version 2 license, then the option applies
  * only if the new code is made subject to such option by the copyright
  * holder.
